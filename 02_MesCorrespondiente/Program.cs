d78ac92fb22d094658f958bf576426563841daf8
Gabriel Del Vacchio <gabrieldelvacchio@gmail.com>
2026-09-09 23:16:01 -0300

    Ejercicio 02: mitad de año segun el mes con switch

---
+++ 02_MesCorrespondiente/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02_MesCorrespondiente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Elaborar un algoritmo que permita indicar si el mes ingresado pertenece a la primera o segunda mitad del año:*/
+
+
+            Console.Write("Ingrese el mes: ");
+            int mes = int.Parse(Console.ReadLine());
+
+            switch (mes){
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    Console.WriteLine("El mes es correspondiente a la primera mitad del año.");
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                    Console.WriteLine("El mes es correspondiente a la segunda mitad del año.");
+                    break;
+
+                default:
+                    Console.WriteLine("El mes NO es correpondiente a ningun mes del año.");
+                    break;
+            }
+
+        }
+    }
+}
