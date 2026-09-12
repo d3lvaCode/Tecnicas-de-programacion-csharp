6a7f1565d54927eab9aab8947b124f5fb6710749
Gabriel Del Vacchio <gabrieldelvacchio@gmail.com>
2026-09-12 03:09:05 -0300

    Ejercicio 13: saldo superiores a 1000 y promedio de los restantes

---
+++ 13_SaldoCuentasDeBanco/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _13_SaldoCuentasDeBanco
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Se ingresan 5 saldos de cuentas de un banco. 
+            Calcular e informar: 
+                    a. Cantidad de saldos superiores a $1000. 
+                    b. Promedio de los restantes. 
+            */
+
+            double saldoMayores = 0, saldosRestantes = 0, sumaRestantes = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese el saldo: ");
+                double saldo = double.Parse(Console.ReadLine());
+
+                if (saldo > 1000)
+                {
+                    saldoMayores++;
+                    //Console.WriteLine("");
+                }
+                else
+                {
+                    saldosRestantes++; // Cuenta cuantos saldos van
+                    sumaRestantes += saldo; // acumula el valor de esos saldos
+                }
+
+               
+
+            }//cierra for
+
+            double promedioRestantes = sumaRestantes / saldosRestantes;
+
+            //INFORMES
+            Console.WriteLine("La cantidad de sueldos con saldo mayor a $1000 son: " + saldoMayores);
+            Console.WriteLine("La cantidad de sueldos con saldo Menor a $1000 son: " + saldosRestantes);
+            Console.WriteLine("El promedio de todos los saldos es: " + promedioRestantes);
+
+
+        }//cierra static
+    }
+}
+    
