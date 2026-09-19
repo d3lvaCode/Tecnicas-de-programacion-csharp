70fcbecf21411b254fa17ecf84d46d2e79b04208
Gabriel Del Vacchio <gabrieldelvacchio@gmail.com>
2026-09-18 22:10:23 -0300

    Ejercicio 18: Ciclos For con Contadores.

---
+++ 17_SumarYcontar/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _17_SumarYcontar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+                 SumarYContar: Escribir un programa que:
+                    a) Pida por teclado un nuumero.
+                    b) Pregunte al usuario si desea introducir otro numero o no.
+                    c) Repita los pasos a y b, mientras que, el usuario no responda ’n’ de (no).
+                    d) Muestre por pantalla cuantos numeros han sido introducidos por el usuario como
+                    tambien la suma de todos ellos.
+                    Nota: Utilizar un bucle while (mientras).
+             */
+
+            double numero, contador = 0, suma = 0;
+            char siOno;
+
+            Console.WriteLine("Quieres ingresar un numero? (S/N): ");
+            siOno = char.Parse(Console.ReadLine());
+
+
+
+
+            while(siOno != 'n' && siOno != 'N')
+            {
+                Console.WriteLine("Ingrese el numero: ");
+                numero = double.Parse(Console.ReadLine());
+                contador++;
+
+                suma += numero;
+
+                Console.WriteLine("Quieres ingresar otro numero? (S/N): ");
+                siOno = char.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine($"Se ingresaron {contador} veces y la suma de los numeros son {suma}");
+
+        }
+    }
+}
---
+++ 18_CicloForContadores/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CicloFOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Ingresar 10 números enteros. Calcular e informar la cantidad de positivos, la cantidad de negativos y la cantidad de ceros.*/
+
+            int contadorPositivos = 0, contadorNegativos = 0, cantidadCeros = 0,  suma = 0, numero, num;
+
+            for(int n = 0; n < 5; n++)
+            {
+                Console.WriteLine("Ingresa numero: ");
+                numero = int.Parse(Console.ReadLine());
+                
+                if (numero > 0)
+                {
+                   contadorPositivos++;
+                }
+                else if (numero < 0)
+                {
+                   contadorNegativos++; 
+                }
+                else
+                {
+                   cantidadCeros++;
+                }
+            }//cierra for
+
+            //Informes
+
+            Console.WriteLine("Cantidad de positivos:" + contadorPositivos);
+            Console.WriteLine("Cantidad de Negativos:" + contadorNegativos);
+            Console.WriteLine("Cantidad de Ceros:" + cantidadCeros);  
+        }
+    }
+}
